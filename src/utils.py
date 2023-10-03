30cc3bda79c5c55023aaffb23f5d85c370e3cf4d
ginofft <ntnghia.andrei@gmail.com>
2023-10-03 23:33:16 +0700

    inrease mask rate

--- src/utils.py
+++ src/utils.py
@@ -7,26 +7,26 @@ import frozendict
 import functools
 
 def normalize_bbox(layout,
-                   resolution_w = 32,
-                   resolution_h = 32):
+				   resolution_w = 32,
+				   resolution_h = 32):
 	"""Normalize the bounding box.
-    
-    Args
-    ----
-    layout : np.array
+	
+	Args
+	----
+	layout : np.array
 		An iterable of normalize bounding box coordinate
-        in the format of (class, width, height, center_x, center_y)
-    resolution_w : int
+		in the format of (class, width, height, center_x, center_y)
+	resolution_w : int
 		the width of the model input
 	resolution_h : int
 		the height of the model input
-    
-    Return
-    ------
-    layout : np.array
+	
+	Return
+	------
+	layout : np.array
 		An iterable of normalize bounding box coordinate 
-        in the format of (class, x_min, y_min, x_max, y_max)
-    """
+		in the format of (class, x_min, y_min, x_max, y_max)
+	"""
 	layout = np.array(layout, dtype=np.float32)
 	layout = np.reshape(layout, (-1, 5))
 	width, height = np.copy(layout[:,1]), np.copy(layout[:, 2])
@@ -174,63 +174,60 @@ def center_similarity(correlated):
 	return correlations + remove_diagonal_entries_mask
 
 def attribute_random_masking(inputs, mask_token, pad_token, layout_dim):
-    """Repace some token with inputs token. 
-    However, for a specific layout, only token with the same semantic meaning are masked.
-    """
-    targets = inputs
-    total_dim = layout_dim*2 + 1
-    rng = jax.random.PRNGKey(jnp.sum(inputs, dtype='int32'))
-        
-    # Get positions (in the tokenized sequence)
-    # - [PAD] token position
-    # - element's class position
-    # - element's width height position
-    # - element's center position
-    is_pad = inputs == pad_token
-    position_ids = jnp.arange(inputs.shape[-1])[None, :]
-    is_asset = position_ids % total_dim == 0
-    is_size = functools.reduce(
-        lambda x,y: x|y,
-        [position_ids % total_dim == i for i in range(1, layout_dim+1)]
-    )
-    is_position = functools.reduce(
-        lambda x,y: x | y,
-        [position_ids % total_dim == i for i in range(layout_dim+1, total_dim)]
-    )
+	"""Repace some token with [mask] token. 
+	However, for a specific layout, only token with the same semantic meaning are masked.
+	"""
+	targets = inputs
+	total_dim = layout_dim*2 + 1
+	rng = jax.random.PRNGKey(jnp.sum(inputs, dtype='int32'))
+		
+	# Get positions (in the tokenized sequence)
+	# - [PAD] token position
+	# - element's class position
+	# - element's width height position
+	# - element's center position
+	is_pad = inputs == pad_token
+	position_ids = jnp.arange(inputs.shape[-1])[None, :]
+	is_asset = position_ids % total_dim == 0
+	is_size = functools.reduce(
+		lambda x,y: x|y,
+		[position_ids % total_dim == i for i in range(1, layout_dim+1)]
+	)
+	is_position = functools.reduce(
+		lambda x,y: x | y,
+		[position_ids % total_dim == i for i in range(layout_dim+1, total_dim)]
+	)
 
-    rand = jax.random.uniform(rng, (inputs.shape[0],1))
-    pre_masked_inputs = inputs.copy()
-    # all token are [MASKED]
-    fullmask = jnp.full_like(inputs, mask_token)
-    fullmask = jnp.where(is_pad, pad_token, fullmask) # [PAD] token
+	rand = jax.random.uniform(rng, (inputs.shape[0],1))
+	pre_masked_inputs = inputs.copy()
+	# all token are [MASKED]
+	fullmask = jnp.full_like(inputs, mask_token)
+	fullmask = jnp.where(is_pad, pad_token, fullmask) # [PAD] token
+	
+	weights = is_asset & (~is_pad)
+	weights = jnp.where(
+		jnp.logical_and(rand >= 0.33, rand < 0.67), is_size & (~is_pad), weights)
+	weights = jnp.where(rand >= 0.67, is_position & (~is_pad), weights)
+	
+	lens = jnp.sum(weights, axis=-1)
+	rng, subrng = jax.random.split(rng)
+	mask_rate = 1 - jax.random.uniform(subrng, lens.shape)
+	mask_rate = jnp.clip(mask_rate, 0.2, 1.)
 
-    weights = is_asset & (~is_pad)
-    weights = jnp.where(
-        jnp.logical_and(rand>=0.2, rand<0.4), is_size & (~is_pad), weights)
-    weights = jnp.where(
-        jnp.logical_and(rand>=0.4, rand<0.6), is_position & (~is_pad), weights)
-    weights = jnp.where(
-        jnp.logical_and(rand>=0.6, rand<0.8), is_size & (~is_pad), weights)
-    weights = jnp.where(rand>0.8, is_asset & ~(is_pad), weights)
-    
-    lens = jnp.sum(weights, axis=-1)
-    rng, subrng = jax.random.split(rng)
-    mask_rate = 1 - jax.random.uniform(subrng, lens.shape)
+	mask_lens = jax.lax.ceil(lens*mask_rate).astype('int32')
+	rng, subrng = jax.random.split(rng)
+	
+	should_mask = jax.random.uniform(subrng, shape=inputs.shape)
+	should_mask = jnp.where(weights, should_mask, 2.)
+	sorted_should_mask = jnp.sort(should_mask, axis=-1)
+	cut_off = jnp.take_along_axis(
+		sorted_should_mask, jnp.expand_dims(mask_lens-1, 1), axis=-1)
+	should_mask = jnp.where(should_mask <= cut_off, 1, 0)
+	fullmask = jnp.full_like(inputs, mask_token)
 
-    mask_lens = jax.lax.ceil(lens*mask_rate).astype('int32')
-    rng, subrng = jax.random.split(rng)
-    
-    should_mask = jax.random.uniform(subrng, shape=inputs.shape)
-    should_mask = jnp.where(weights, should_mask, 2.)
-    sorted_should_mask = jnp.sort(should_mask, axis=-1)
-    cut_off = jnp.take_along_axis(
-        sorted_should_mask, jnp.expand_dims(mask_lens-1, 1), axis=-1)
-    should_mask = jnp.where(should_mask <= cut_off, 1, 0)
-    fullmask = jnp.full_like(inputs, mask_token)
-
-    masked_inputs = jnp.where(should_mask, fullmask, pre_masked_inputs)
-    weights = jnp.where(is_pad, 0, should_mask)
-    return dict(masked_inputs=masked_inputs, targets=targets, weights=weights)
+	masked_inputs = jnp.where(should_mask, fullmask, pre_masked_inputs)
+	weights = jnp.where(is_pad, 0, should_mask)
+	return dict(masked_inputs=masked_inputs, targets=targets, weights=weights)
 
 def get_magazine_config():
 	"""Gets the default hyperparameter configuration."""
