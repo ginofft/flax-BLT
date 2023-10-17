11bdf6e198d293b532e96be786d62205afbac636
ginofft <ntnghia.andrei@gmail.com>
2023-10-17 09:29:30 +0700

    best architecture found ?

--- src/utils.py
+++ src/utils.py
@@ -320,7 +320,7 @@ def get_obello_config():
 	config.share_embeddings = True
 	config.num_layers = 4
 	config.qkv_dim = 512
-	config.mlp_dim = 2048
+	config.mlp_dim = 1024
 	config.num_heads = 8
 	config.dropout_rate = 0.5
 	config.attention_dropout_rate = 0.5
