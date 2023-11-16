2c887f4bb8ac7994f08bca0667f486e716d98ff6
ginofft <ntnghia.andrei@gmail.com>
2023-11-16 11:37:40 +0700

    decrease complexity

--- src/utils.py
+++ src/utils.py
@@ -285,9 +285,9 @@ def get_obello_config():
 	config.share_embeddings = True
 	config.num_layers = 4
 	config.qkv_dim = 512
-	config.mlp_dim = 1024
+	config.mlp_dim = 512
 	config.num_heads = 8
-	config.dropout_rate = 0.3
+	config.dropout_rate = 0.1
 	config.attention_dropout_rate = 0.3
 	config.label_smoothing = 0.
 	config.sampling_method = "greedy"
