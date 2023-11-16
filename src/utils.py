f969a4d346aceaa3140169d53beb4acae67107eb
ginofft <ntnghia.andrei@gmail.com>
2023-11-16 21:09:03 +0700

    hmm

--- src/utils.py
+++ src/utils.py
@@ -285,7 +285,7 @@ def get_obello_config():
 	config.share_embeddings = True
 	config.num_layers = 4
 	config.qkv_dim = 512
-	config.mlp_dim = 512
+	config.mlp_dim = 1024
 	config.num_heads = 16
 	config.dropout_rate = 0.1
 	config.attention_dropout_rate = 0.3
