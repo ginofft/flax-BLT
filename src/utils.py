9ba429452cd8a4f388c6f82fb3f7c775ae4c17b9
ginofft <ntnghia.andrei@gmail.com>
2023-11-18 14:43:38 +0700

    add only mask size, position

--- src/utils.py
+++ src/utils.py
@@ -68,48 +68,102 @@ def attribute_random_masking(inputs, mask_token, pad_token, layout_dim):
 	# - element's class position
 	# - element's width height position
 	# - element's center position
-	is_pad = inputs == pad_token
+	is_pad = inputs == pad_token # [batch size, seq_len]
 	position_ids = jnp.arange(inputs.shape[-1])[None, :]
-	is_asset = position_ids % total_dim == 0
+	is_asset = position_ids % total_dim == 0 # [1, seq_len]
 	is_size = functools.reduce(
 		lambda x,y: x|y,
-		[position_ids % total_dim == i for i in range(1, layout_dim+1)]
-	)
+		[position_ids % total_dim == i for i in range(1, layout_dim+1)]) # [1, seq_len] 
 	is_position = functools.reduce(
 		lambda x,y: x | y,
-		[position_ids % total_dim == i for i in range(layout_dim+1, total_dim)]
-	)
-
-	rand = jax.random.uniform(rng, (inputs.shape[0],1))
+		[position_ids % total_dim == i for i in range(layout_dim+1, total_dim)]) # [1, seq_len] 
+	rand = jax.random.uniform(rng, (inputs.shape[0],1)) # [batch size, 1]
+	# There dimension are as they are to support forecasting 
 	pre_masked_inputs = inputs.copy()
+	
 	# all token are [MASKED]
 	fullmask = jnp.full_like(inputs, mask_token)
-	fullmask = jnp.where(is_pad, pad_token, fullmask) # [PAD] token
+	fullmask = jnp.where(is_pad, pad_token, fullmask) # [PAD] token 
 	
-	weights = is_asset & (~is_pad)
-	weights = jnp.where(
-		jnp.logical_and(rand >= 0.33, rand < 0.67), is_size & (~is_pad), weights)
-	weights = jnp.where(rand >= 0.67, is_position & (~is_pad), weights)
+	# Whether to mask asset, size or position
+	weights = is_asset & (~is_pad) # Mask every asset
+	weights = jnp.where(jnp.logical_and(rand >= 0.33, rand < 0.67), is_size & (~is_pad), weights) # if [condition], mask size
+	weights = jnp.where(rand >= 0.67, is_position & (~is_pad), weights) # if [condition], mask position
 	
-	lens = jnp.sum(weights, axis=-1)
+	lens = jnp.sum(weights, axis=-1) # [batch size] , no. POSSIBLE mask token per layout
 	rng, subrng = jax.random.split(rng)
-	mask_rate = 1 - jax.random.uniform(subrng, lens.shape)
+	mask_rate = 1 - jax.random.uniform(subrng, lens.shape) # [batch size], mask rate of each layout
 
-	mask_lens = jax.lax.ceil(lens*mask_rate).astype('int32')
-	rng, subrng = jax.random.split(rng)
+	mask_lens = jax.lax.ceil(lens*mask_rate).astype('int32') # [batch size], no. ACTUAL mask token per layout
+	rng, subrng = jax.random.split(rng) 
 	
-	should_mask = jax.random.uniform(subrng, shape=inputs.shape)
-	should_mask = jnp.where(weights, should_mask, 2.)
-	sorted_should_mask = jnp.sort(should_mask, axis=-1)
+	should_mask = jax.random.uniform(subrng, shape=inputs.shape) # [batch size, seq_len], probability of being masked
+	# If weights is true (can be mask for this layout), keep the probability, else equal 2 -> prevent being masked
+	should_mask = jnp.where(weights, should_mask, 2.) # [batch size, seq_len]
+	sorted_should_mask = jnp.sort(should_mask, axis=-1) # [batch size, seq_len] sorted version
 	cut_off = jnp.take_along_axis(
-		sorted_should_mask, jnp.expand_dims(mask_lens-1, 1), axis=-1)
-	should_mask = jnp.where(should_mask <= cut_off, 1, 0)
-	fullmask = jnp.full_like(inputs, mask_token)
+		sorted_should_mask, jnp.expand_dims(mask_lens-1, 1), axis=-1) # [batch size, 1] the cut off of each layout
+	should_mask = jnp.where(should_mask <= cut_off, 1, 0) # [batch size, seq_len] where there should be masked
 
-	masked_inputs = jnp.where(should_mask, fullmask, pre_masked_inputs)
-	weights = jnp.where(is_pad, 0, should_mask)
+	masked_inputs = jnp.where(should_mask, fullmask, pre_masked_inputs) # [batch size, seq_len] masked input
+	weights = jnp.where(is_pad, 0, should_mask) 
 	return dict(masked_inputs=masked_inputs, targets=targets, weights=weights)
 
+def attribute_random_masking(inputs, mask_token, pad_token, layout_dim):
+	"""Repace some token with [mask] token. 
+	However, for a specific layout, only token with the same semantic meaning are masked.
+	We only mask position and size here.
+	"""
+	targets = inputs
+	total_dim = layout_dim*2 + 1
+	rng = jax.random.PRNGKey(jnp.sum(inputs, dtype='int32'))
+		
+	# Get positions (in the tokenized sequence)
+	# - [PAD] token position
+	# - element's class position
+	# - element's width height position
+	# - element's center position
+	is_pad = inputs == pad_token # [batch size, seq_len]
+	position_ids = jnp.arange(inputs.shape[-1])[None, :]
+	is_asset = position_ids % total_dim == 0 # [1, seq_len]
+	is_size = functools.reduce(
+		lambda x,y: x|y,
+		[position_ids % total_dim == i for i in range(1, layout_dim+1)]) # [1, seq_len] 
+	is_position = functools.reduce(
+		lambda x,y: x | y,
+		[position_ids % total_dim == i for i in range(layout_dim+1, total_dim)]) # [1, seq_len] 
+	rand = jax.random.uniform(rng, (inputs.shape[0],1)) # [batch size, 1]
+	# There dimension are as they are to support forecasting 
+	pre_masked_inputs = inputs.copy()
+	
+	# all token are [MASKED]
+	fullmask = jnp.full_like(inputs, mask_token)
+	fullmask = jnp.where(is_pad, pad_token, fullmask) # [PAD] token 
+	
+	# Whether to mask asset, size or position
+	weights = (~is_pad) # Mask every asset
+	weights = jnp.where(rand <= 0.5, is_size & (~is_pad), weights) # if [condition], mask size
+	weights = jnp.where(rand > 0.5, is_position & (~is_pad), weights) # if [condition], mask position
+	
+	lens = jnp.sum(weights, axis=-1) # [batch size] , no. POSSIBLE mask token per layout
+	rng, subrng = jax.random.split(rng)
+	mask_rate = 1 - jax.random.uniform(subrng, lens.shape) # [batch size], mask rate of each layout
+
+	mask_lens = jax.lax.ceil(lens*mask_rate).astype('int32') # [batch size], no. ACTUAL mask token per layout
+	rng, subrng = jax.random.split(rng) 
+	
+	should_mask = jax.random.uniform(subrng, shape=inputs.shape) # [batch size, seq_len], probability of being masked
+	# If weights is true (can be mask for this layout), keep the probability, else equal 2 -> prevent being masked
+	should_mask = jnp.where(weights, should_mask, 2.) # [batch size, seq_len]
+	sorted_should_mask = jnp.sort(should_mask, axis=-1) # [batch size, seq_len] sorted version
+	cut_off = jnp.take_along_axis(
+		sorted_should_mask, jnp.expand_dims(mask_lens-1, 1), axis=-1) # [batch size, 1] the cut off of each layout
+	should_mask = jnp.where(should_mask <= cut_off, 1, 0) # [batch size, seq_len] where there should be masked
+
+	masked_inputs = jnp.where(should_mask, fullmask, pre_masked_inputs) # [batch size, seq_len] masked input
+	weights = jnp.where(is_pad, 0, should_mask) 
+	return dict(masked_inputs=masked_inputs, targets=targets, weights=weights)
+	
 def get_magazine_config():
 
 	"""Gets the default hyperparameter configuration."""
