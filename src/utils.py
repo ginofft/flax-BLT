77435fccef85911c0dd9859eea041354260a1608
ginofft <ntnghia.andrei@gmail.com>
2023-11-20 12:40:01 +0700

    stuff

--- src/utils.py
+++ src/utils.py
@@ -243,7 +243,7 @@ def get_publaynet_config():
 	config.image_size = 256
 
 	# Training info
-	config.epoch = 700
+	config.epoch = 500
 	config.seed = 0
 	config.max_length = 130
 	config.batch_size = 64
