76b170faab3fe28697f920c99d42c7b089f2f39e
ginofft <ntnghia.andrei@gmail.com>
2023-11-29 10:08:48 +0700

    smaller architecture for 1 template case

--- src/utils.py
+++ src/utils.py
@@ -336,10 +336,10 @@ def get_obello_config():
 	config.shuffle_buffer_size = 10
 	config.use_vae = True
 	config.share_embeddings = True
-	config.num_layers = 4
+	config.num_layers = 2
 	config.qkv_dim = 512
-	config.mlp_dim = 1024
-	config.num_heads = 8
+	config.mlp_dim = 512
+	config.num_heads = 4
 	config.dropout_rate = 0.1
 	config.attention_dropout_rate = 0.3
 	config.label_smoothing = 0.
